d51de0a3c55cd935388b45614d9497513162ea77
Ritesh Kumar <ritesh.kumar.07.20@gmail.com>
2026-02-24 00:29:17 +0100

    added loading widget

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,44 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:portfolio/responsive.dart';
 import 'package:portfolio/screens/desktop/desktopHomepage.dart';
 import 'package:portfolio/screens/mobile/mobileHomepage.dart';
 import 'package:portfolio/screens/tablet/tabletHomepage.dart';
 
-class Homepage extends StatefulWidget {
+class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  bool _isLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      setState(() {
-        _isLoaded = true;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (!_isLoaded) {
-      return Scaffold(
-        body: Center(
-          child: Lottie.asset(
-            'assets/progressive tapping.lottie',
-            width: 150,
-            height: 150,
-          ),
-        ),
-      );
-    }
-
     return const Responsive(
       desktop: DesktopHomepage(),
       mobile: MobileHomepage(),
